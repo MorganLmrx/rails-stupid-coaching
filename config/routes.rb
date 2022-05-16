77e7559cc4cd86e96a4b8d034a1c06731129856a
Morgan Lemoroux <morganlemoroux@gmail.com>
2022-05-16 15:08:03 +0200

    exo 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'ask', to: 'questions#ask'
+  get 'answer', to: 'questions#answer'
+  get 'path', to: 'questions#questions_path'
   # Defines the root path route ("/")
   # root "articles#index"
 end
